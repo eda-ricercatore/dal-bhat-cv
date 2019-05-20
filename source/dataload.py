4649da5ccf1c7537ddc7e2f1c8b4eabb2b6b9066
Sournav Sekhar Bhattacharya <32916097+sournav@users.noreply.github.com>
2019-05-19 20:27:49 -0500

    dataloader working no comments

--- source/dataload.py
+++ source/dataload.py
@@ -3,17 +3,8 @@ import sys,os
 import xml.etree.ElementTree as ET
 import numpy as np
 print(os.listdir())
-cat = []
-dirs = os.listdir()
-for i in os.listdir():
-    
-    cat.append(''.join(list(filter(lambda x: x.isalpha(),i))))
-cat = list(set(cat))
-cat.remove('dataloadpy')
-cat.remove('group')
-images = []
-lables = []
-bounding_box = []
+
+
 def get_coords(path):
     root = ET.parse(path).getroot()
     bbt = ['xmax','xmin','ymax','ymin']
@@ -23,10 +14,33 @@ def get_coords(path):
         bbx.append(int(val.text))
     return bbx
 def get_label(cat,lbl_list):
+    clean_cat =''.join(list(filter(lambda x: x.isalpha(),cat)))
     one_hot = np.zeros(len(lbl_list))
-    one_hot[lbl_list.index(cat)] = 1
+    one_hot[lbl_list.index(clean_cat)] = 1
     return one_hot
-print(get_label('car',cat))
+
+def proc():
+    cat = []
+    dirs = os.listdir()
+    for i in os.listdir():
+    
+        cat.append(''.join(list(filter(lambda x: x.isalpha(),i))))
+    
+    cat = list(set(cat))
+    cat.remove('dataloadpy')
+    dirs.remove('dataload.py')
+    images = []
+    labels = []
+    bbs = []
     
     
-get_coords(dirs[0]+'/000001.xml')
+    for folder in dirs:
+        files = os.listdir(folder)
+        files = [i[:-4] for i in files if i.endswith('.xml')]
+        for file in files:
+            images.append(cv2.imread(folder+'/'+file+'.jpg'))
+            labels.append(get_label(folder,cat))
+            bbs.append(get_coords(folder+'/'+file+'.xml'))
+            print(bbs,labels)
+    return bbs,labels,imgs
+proc()
